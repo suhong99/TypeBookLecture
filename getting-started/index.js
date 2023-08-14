e85c3c5b3b1b48db2d214178f6758423da39f321
SUHONG\topic <topic828@Naver.com>
2023-08-14 21:10:45 +0900

    11장까지

--- getting-started/index.js
+++ getting-started/index.js
@@ -1,4 +1,13 @@
-function sum(a, b) {
-  return a + b;
+function sum2(a, b) {
+    return a + b;
 }
-console.log(sum(10, 20));
+//console.log(sum(10, 20));
+var Person = /** @class */ (function () {
+    function Person(name, skill) {
+        this.name = name;
+        this.skill = skill;
+    }
+    return Person;
+}());
+var capt = new Person("캡틴", "방패던지기");
+console.log(capt.name);
--- getting-started/index.ts
+++ getting-started/index.ts
@@ -1,5 +1,18 @@
-function sum(a: number, b: number) {
+function sum2(a: number, b: number) {
   return a + b;
 }
 
-console.log(sum(10, 20));
+//console.log(sum(10, 20));
+
+class Person {
+  name: string;
+  skill: string;
+
+  constructor(name: string, skill: string) {
+    this.name = name;
+    this.skill = skill;
+  }
+}
+
+var capt = new Person('캡틴', '방패던지기');
+console.log(capt.name);
